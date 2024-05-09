7eff0c8da56f0e0afbbee48713dde41e4908c15b
Ali Vijdaan <alivijdaan@gmail.com>
2024-05-10 04:44:38 +0500

    added local testing function

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
+import cv2
 import pdb
 # Load MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -16,7 +17,8 @@ y_test = tf.keras.utils.to_categorical(y_test, 10)
 
 # Build the model
 model = Sequential()
-model.add(Dense(512, activation='relu', input_shape=(784,)))
+model.add(Dense(512, activation='relu', input_shape=(784,), kernel_regularizer=tf.keras.regularizers.l2(0.01)))
+model.add(Dropout(0.4))
 model.add(Dense(10, activation='softmax'))
 
 # Compile the model
@@ -25,10 +27,28 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 # Train the model
-model.fit(x_train, y_train, batch_size=128, epochs=5, verbose=1, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, batch_size=128, epochs=6, verbose=1, validation_data=(x_test, y_test))
 
 # Evaluate the model
 loss, accuracy = model.evaluate(x_test, y_test, verbose=0)
 print(f'Test loss: {loss:.3f}, Test accuracy: {accuracy:.3f}')
+
+def test_model_with_image(model, image_path):
+    # Load the image
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (28, 28), interpolation=cv2.INTER_LINEAR)
+    img = cv2.bitwise_not(img)
+    img = img.reshape(1, 784)
+    img = img.astype('float32') / 255
+    # Make predictions
+    predictions = model.predict(img)
+
+    # Get the index of the highest probability
+    index = np.argmax(predictions)
+
+    # Print the result
+    print(f'The image is predicted to be: {index}')
+    print(predictions)
 
 model.save("mnist_app.h5")
