34df2bd82427c9ffa07e92da9874a070446b338f
Ali Vijdaan <alivijdaan@gmail.com>
2024-05-10 04:46:12 +0500

    minor bug fixes

--- neuralNetwork.py
+++ neuralNetwork.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
+import numpy as np
 import cv2
 import pdb
 # Load MNIST dataset
